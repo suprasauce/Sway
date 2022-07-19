fa41e049203caf91396f90428306f601cfdc69a2
GURJYOT BHULLAR <gjbhullar16@gmail.com>
2022-07-19 22:53:03 +0530

    Add parabolic motion to the bob

--- entities/bob.py
+++ entities/bob.py
@@ -8,6 +8,7 @@ class bob:
         self.height = constants.BOB_HEIGHT
         self.color = colors.GREEN
         self.x, self.y = self.get_bob_pos(ep_x, ep_y)
+        self.v_x, self.v_y = 0.0, 0.0
 
     def get_bob_pos(self,ep_x, ep_y):
         return [ep_x - self.width/2, ep_y]
@@ -15,12 +16,18 @@ class bob:
     def get_rect(self):
         return py.Rect(self.x, self.y, self.width, self. height)
 
-    def draw(self, screen, ep_x, ep_y):
-        self.x, self.y = self.get_bob_pos(ep_x, ep_y)
+    def draw(self, screen, is_bob_free, ep_x = 0.0, ep_y = 0.0):
+        if not is_bob_free:
+            self.x, self.y = self.get_bob_pos(ep_x, ep_y)
         py.draw.rect(screen, self.color, self.get_rect())
 
-    def parabolic_motion(self):
-        pass
+    def set_parabolic_motion_initials(self, v_x, v_y):
+        self.v_x, self.v_y = v_x, v_y
+
+    def move(self):
+        self.x += self.v_x
+        self.y -= self.v_y
+        self.v_y -= 0.8
 
     def is_collision(self):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import pygame as py
 import numpy as np
 import colors
@@ -14,30 +15,50 @@ screen_width, screen_height = py.display.get_surface().get_size()
 clock = py.time.Clock()
 
 def main():
+    
     run = True
-    free = True
+    is_pen_free = True
+    is_bob_free = False
     pivot = (screen_width/4, screen_height/3)
     ep_x, ep_y = screen_width/4, 2*screen_height/3
     end_point = ep(ep_x, ep_y, pivot)
     bob =b(ep_x, ep_y)
     arc = ar([screen_width, screen_height])
+
     while(run):
+
         clock.tick(fps)
         screen.fill(colors.WHITE)
         for event in py.event.get():
             if event.type == py.QUIT:
                 run = False
             elif event.type == py.MOUSEBUTTONDOWN:
-                free = False
+                is_pen_free = False
             elif event.type == py.MOUSEBUTTONUP:
-                free = True
-        if free:
-            end_point.move()
+                is_pen_free = True
+            elif event.type == py.KEYDOWN:
+                if event.key == py.K_SPACE and not is_bob_free:
+                    is_bob_free = True
+                    curr_velocity = end_point.theta2*end_point.length
+                    curr_angle = end_point.theta1
+                    v_x = curr_velocity*math.cos(curr_angle)
+                    v_y = curr_velocity*math.sin(curr_angle)
+                    bob.set_parabolic_motion_initials(v_x/2, v_y/2)
+
+        if not is_bob_free:
+            if is_pen_free:
+                end_point.move()
+            else:
+                new_x, new_y = py.mouse.get_pos()
+                end_point.reset_attributes(new_x, new_y)
+
+        if is_bob_free:
+            bob.move()
+            bob.draw(screen, is_bob_free=is_bob_free)
         else:
-            new_x, new_y = py.mouse.get_pos()
-            end_point.reset_attributes(new_x, new_y)
-        bob.draw(screen, end_point.x, end_point.y)
-        py.draw.line(screen, colors.GREEN, pivot, [end_point.x, end_point.y],1)
+            bob.draw(screen,  is_bob_free, end_point.x, end_point.y)
+            py.draw.line(screen, colors.GREEN, pivot, [end_point.x, end_point.y],1)
+
         arc.rotate()
         arc.draw(screen)
         py.display.update()
